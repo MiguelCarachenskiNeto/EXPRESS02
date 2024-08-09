cefb7239cc551206112b3782ce326121ce1f7cce
Miguel <miguel.carachenski.neto@escola.pr.gov.br>
2024-08-09 12:15:43 -0300

    first commst

--- server6.js
+++ server6.js
@@ -17,8 +17,9 @@ app.get("/add/:a/:b",(req,res)=>{
 
     const resultado = funcoes.add(a,b);
 
-    res.send(`A soma de ${a} com ${b} é ${resultado}`)
+    res.send(`A soma entre ${a} e ${b} é ${resultado}`)
 })
+
 app.get("/sub/:a/:b",(req,res)=>{
     const a = parseInt(req.params.a)
     const b = parseInt(req.params.b)
@@ -26,4 +27,22 @@ app.get("/sub/:a/:b",(req,res)=>{
     const resultado = funcoes.sub(a,b);
 
     res.send(`A subtração entre ${a} e ${b} é ${resultado}`)
+})
+
+app.get("/mult/:a/:b",(req,res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)
+
+    const resultado = funcoes.mult(a,b);
+
+    res.send(`A multiplicação entre ${a} e ${b} é ${resultado}`)
+})
+
+app.get("/div/:a/:b",(req,res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)
+
+    const resultado = funcoes.div(a,b);
+
+    res.send(`A divisão de ${a} por ${b} é ${resultado}`)
 })
